ce062a3673ddb827c7f38001344e5ff8641e3a9e
Lukáš Varga <lukas.varga128@gmail.com>
2021-01-21 00:41:37 +0100

    Finalizing navigation draweru Has similiar functionality as menu on the top right side

--- app/src/main/java/com/oth/archeology/views/sitelist/SiteListView.kt
+++ app/src/main/java/com/oth/archeology/views/sitelist/SiteListView.kt
@@ -4,14 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
 import com.oth.archeology.R
 import com.oth.archeology.models.SiteModel
 import com.oth.archeology.views.BaseView
 import kotlinx.android.synthetic.main.activity_site_list.*
 
-class   SiteListView  : BaseView(), SiteListener {
+
+class   SiteListView  : BaseView(), SiteListener, NavigationView.OnNavigationItemSelectedListener{
 
     lateinit var presenter: SiteListPresenter
     lateinit var menuList: Menu
@@ -20,6 +23,13 @@ class   SiteListView  : BaseView(), SiteListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_site_list)
         super.init(toolbar, false)
+
+        toolbar.setNavigationOnClickListener(){
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        navigationView.setNavigationItemSelectedListener(this)
+
 
         presenter = initPresenter(SiteListPresenter(this)) as SiteListPresenter
 
@@ -48,16 +58,16 @@ class   SiteListView  : BaseView(), SiteListener {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
         when (item?.itemId) {
             R.id.item_add -> presenter.doAddSite()
             R.id.item_map -> presenter.doShowSiteMap()
             R.id.item_favourite -> {
                 var fav: MenuItem = menuList.findItem(R.id.item_favourite)
-                if(fav.title == getString(R.string.menu_showFavourite)){
+                if (fav.title == getString(R.string.menu_showFavourite)) {
                     presenter.doShowFavourites()
                     fav.setTitle(R.string.menu_showAll)
-                }
-                else if(fav.title == getString(R.string.menu_showAll)){
+                } else if (fav.title == getString(R.string.menu_showAll)) {
                     presenter.doLoadSites()
                     fav.setTitle(R.string.menu_showFavourite)
                 }
@@ -66,6 +76,28 @@ class   SiteListView  : BaseView(), SiteListener {
             R.id.item_settings -> presenter.doSettings()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.drawer_SiteAdd -> presenter.doAddSite()
+            R.id.drawer_map -> presenter.doShowSiteMap()
+            R.id.drawer_favourite -> {
+                var menu = navigationView.menu
+                var fav: MenuItem = menu.findItem(R.id.drawer_favourite)
+                if (fav.title == getString(R.string.menu_showFavourite)) {
+                    presenter.doShowFavourites()
+                    fav.setTitle(R.string.menu_showAll)
+                } else if (fav.title == getString(R.string.menu_showAll)) {
+                    presenter.doLoadSites()
+                    fav.setTitle(R.string.menu_showFavourite)
+                }
+            }
+            R.id.drawer_logout -> presenter.doLogout()
+            R.id.drawer_settings -> presenter.doSettings()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 
     //edit existing site
